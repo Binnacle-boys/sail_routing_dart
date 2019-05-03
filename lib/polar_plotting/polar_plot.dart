dacf2c2632e576c3c11d8e7379f4bbfbae73d7b4
nkalscheuer <nkalscheuer@gmail.com>
2019-05-02 17:28:38 -0700

    Adding functionality for getting the csv file

--- lib/polar_plotting/polar_plot.dart
+++ lib/polar_plotting/polar_plot.dart
@@ -1,6 +1,15 @@
 import 'package:csv/csv.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 // Polar plot class generated through csv polar plot files
 class PolarPlot {
+
+  Future init (String filepath) async {
+    final input = new File(filepath).openRead();
+    final fields = await input.transform(utf8.decoder).transform(new CsvToListConverter()).toList();
+    print(fields.toString());
+  }
   void hello() {
     print("Working");
   }
--- test/polar_plot_cli.dart
+++ test/polar_plot_cli.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
-import 'dart:convert';
 import 'dart:async';
 import 'package:sail_routing_dart/polar_plotting/polar_plot.dart';
 
-void main (List<String> arguments) {
+Future main (List<String> arguments) async {
     exitCode = 0;
     print("hey look at my arguments! ");
     for (final arg in arguments) {
         print(arg);
     }
     var plot = PolarPlot();
+    await plot.init(arguments[0]);
     plot.hello();
     
 }
