36b1c387bb9927843462fe28b9ea71cc04062a51
nkalscheuer <nkalscheuer@gmail.com>
2019-05-10 22:36:42 -0700

    Adding some comments to the code

--- lib/polar_algs/polar_router.dart
+++ lib/polar_algs/polar_router.dart
@@ -1,3 +1,5 @@
+/// Polar Router
+/// Algorithm class that gets a route based on a polar plot
 import 'package:sail_routing_dart/polar_plotting/polar_plot.dart';
 import 'package:sail_routing_dart/shared/route_math.dart';
 import 'dart:math';
@@ -11,7 +13,15 @@ class PolarRouter {
     _polarPlot = plot;
   }
 
-  
+  /// Get route that has only been tested with wind at 180 degrees; 
+  /// should transform based on wind direction to handle other cases.
+  /// Arguments:
+  ///   (List<double>) start - Starting point in cartesian space
+  ///   (List<double>) end - End point where boat wants to go
+  ///   (double) windSpeed - Wind speed in knots
+  ///   (double) windDirection - Wind direction - Cardinal directions in degrees
+  /// Returns:
+  ///   (List<List<double>>) - List of points that specify where the boat should go   
   List<List<double>> getTransRoute(
     List<double> start,
     List<double> end,
@@ -21,25 +31,27 @@ class PolarRouter {
     var route = List<List<double>>();
     route.add(start);
     // Get the route
+
+    // Get the desired angle
     double desiredAngle = atan2(end[1] - start[1], end[0] - start[0]);
-    print("desiredAngle: ${desiredAngle}");
     double desiredDirection = radiansToCardinal(desiredAngle);
 
-    print("Desired direction: ${desiredDirection}");
-    print("Wind direction: ${windDirection}");
+    // Get optimal direction
     double vmgDirection = _polarPlot.getOptimalAngle(desiredDirection, windDirection, windSpeed);
+    
+    // Evaluate if straight from a to b is more optimal than taking tack
     double desiredDiff = angleDifference(vmgDirection, desiredDirection).abs();
     bool desiredPossible = ! _polarPlot.inNogoZone(desiredDirection, windDirection, windSpeed);
-    print("vmgDirection");
-    print(vmgDirection);
-    print("Desired Possible: ");
-    print(desiredPossible);
     if (desiredDiff > minDiff || !desiredPossible) {
+      print("Should tack...");
+      print("vmg direction: ${vmgDirection}");
+      print("desiredDirection: ${desiredDirection}");
+      print("Desired Difference: ${desiredDiff} ");
+      print("DesiredPossible = ${desiredPossible}");
       // Case: Should tack
       // Construct vectors
-      print("Adding extra point");
       Vector2 startVec = Vector2(start[0], start[1]);
-      Vector2 endVec =Vector2(end[0], end[1]);
+      Vector2 endVec = Vector2(end[0], end[1]);
 
       double vmgAngle = cardinalTransform(vmgDirection);
       double windAngle = cardinalTransform(windDirection);
@@ -59,6 +71,7 @@ class PolarRouter {
         start, [startVec2.x, startVec2.y], end, [endVec2.x, endVec2.y]);
       route.add(intersect);
     }
+    // End should always be the last point
     route.add(end);
     return route;
   }
--- lib/shared/route_math.dart
+++ lib/shared/route_math.dart
@@ -1,7 +1,37 @@
+/// Common Route Math used in Sailing Algorithms
+/// Author: Nicholas Kalscheuer
 import 'dart:math';
 import 'package:vector_math/vector_math.dart';
 
 
+/// Finds the intersection of 2 lines, p1 & p2 create one line and p3 & p4 make up the other
+/// Uses vectors rather than Lists
+/// Arguments:
+///   (Vector2) p1 - A point on the first line 
+///   (Vector2) p2 - A point on the first line 
+///   (Vector2) p3 - A point on the second line 
+///   (Vector2) p4 - A point on the second line 
+/// Returns:
+///   (Vector2) point where the two lines intersect 
+Vector2 findVecIntersection(
+  Vector2 p1,
+  Vector2 p2,
+  Vector2 p3,
+  Vector2 p4
+) {
+  List<double> intPoint =findIntersection([p1.x,p1.y], [p2.x, p2.y], [p3.x, p3.y], [p4.x, p4.y]);
+  return Vector2(intPoint[0], intPoint[1]);
+}
+
+/// Finds the intersection of 2 lines, p1 & p2 create one line and p3 & p4 make up the other
+/// 
+/// Arguments:
+///   (List<double>) p1 - A point on the first line ([0] - x,  [1] - y)
+///   (List<double>) p2 - A point on the first line ([0] - x,  [1] - y)
+///   (List<double>) p3 - A point on the second line ([0] - x,  [1] - y)
+///   (List<double>) p4 - A point on the second line ([0] - x,  [1] - y)
+/// Returns:
+///   (List<double>) point where the two lines intersect ([0] - x,  [1] - y)
 List<double> findIntersection(
   List<double> p1,
   List<double> p2,
@@ -29,18 +59,30 @@ List<double> findIntersection(
   return [px, py];
 }
 
+/// Transforms back or forward from cardinal directions to polar space - works both ways
+/// Arguments:
+///   (double) angle - Angle in degrees from either polar space or cardinal direction
+/// Returns:
+///   (double) angle - The opposite in degrees
 double cardinalTransform(double angle) {
   return (90 + (360 - angle)) % 360;
 }
 
+/// Transforms back or forward from cardinal directions to polar space - works both ways
+/// Arguments:
+///   (double) angle - Angle in radians from either polar space or cardinal direction
+/// Returns:
+///   (double) angle - The opposite in degrees
 double radiansToCardinal(double rads) {
   return cardinalTransform(radToDeg(rads));
 }
 
+/// Converts degrees to radians
 double degToRad(double degrees) {
   return degrees * pi / 180.0;
 }
 
+/// Converts radians to degrees
 double radToDeg(double rads) {
   return rads * 180.0 / pi;
 }
@@ -61,11 +103,30 @@ double angleDifference(firstAngle, secondAngle) {
   return difference;
 }
 
+/// Creates a vector based on the polar angle of dergrees
+/// Arguments:
+///   (double) degrees - Angle of degrees in standard polar space
+/// Returns:
+///   (Vector2) A unit vector based on the angle given 
 Vector2 polarDegVector(double degrees) {
   double rads = degToRad(degrees);
   return polarRadVector(rads);
 }
 
+/// Creates a vector based on the polar angle in radians
+/// Arguments:
+///   (double) rads - Radians of what the vector will be pointed in polar space
+/// Returns:
+///   (Vector2) A unit vector based on the angle given
 Vector2 polarRadVector(double rads) {
   return Vector2(cos(rads), sin(rads));
+}
+
+/// Generates a List of length 2 from a Vector2
+/// Arguments:
+///   (Vector2) vec - 2 dimensional vector that will be used to make the list
+/// Returns:
+///   (List<double>) List with x at index 0, y at index 1 
+List<double> listToVec(Vector2 vec) {
+  return [vec.x, vec.y];
 }
